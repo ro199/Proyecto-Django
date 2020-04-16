0c3f23ed2d463bf5ab740847c1d905e422f9a63f
ro199 <49215567+ro199@users.noreply.github.com>
2020-04-16 17:31:19 -0500

    Modulo 8

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Meeting, Room
+from .forms import MeetingForm
+
 # Create your views here.
 def detail(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
@@ -9,3 +11,12 @@ def detail(request, id):
 
 def rooms_list(request):
     return render(request, "meetings/rooms_list.html ", {"rooms": Room.objects.all()})
+
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
